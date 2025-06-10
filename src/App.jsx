d138f1091b0b03c23b1ecce0541cbb8a0254b061
code1218 <skjil1218@gmail.com>
2025-06-10 17:52:00 +0900

    react 기초

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+import HelloJsx from './study/components/HelloJsx/HelloJsx'
+import HelloProps from './study/components/HelloProps/HelloProps'
+import HelloReact from './study/components/HelloReact/HelloReact'
+import Calculator from './study/components/State/Calculator/Calculator'
+import CountState from './study/components/State/CountState/CountState'
+import InputState1 from './study/components/State/InputState1/InputState1'
+import InputState2 from './study/components/State/InputState2/InputState2'
+
+function App() {
+
+  return <div>
+    {/* <HelloReact /> */}
+    {/* <HelloJsx /> */}
+    {/* <HelloProps /> */}
+    {/* <CountState /> */}
+    {/* <Calculator /> */}
+    {/* <InputState1 /> */}
+    <InputState2 />
+  </div>
+}
+
+export default App
