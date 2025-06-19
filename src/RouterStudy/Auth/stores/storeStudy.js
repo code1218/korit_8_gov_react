8bb66d333a65985163b72025edc45ac820a6687b
code1218 <skjil1218@gmail.com>
2025-06-19 17:51:39 +0900

    login

---
+++ src/RouterStudy/Auth/stores/storeStudy.js
@@ -0,0 +1,18 @@
+import { create } from "zustand";
+
+export const useRefreshStore = create((setter) => ({
+    value: true,
+    setValue: (callback) => setter(state => ({value: callback(state)})),
+}));
+
+export const useGlobalStateStore = create((set) => ({
+    name: "김준일",
+    setName: (newName) => set(state => ({name: newName})),
+    setName2: () => set(state => ({name: state.name + "님"}))
+}));
+
+// export const useRefreshStore = create((set) => ({
+//     isRefresh: true,
+//     refresh: () => set(state => ({isRefresh: true})),
+//     reset: () => set(state => ({isRefresh: false})),
+// }));
